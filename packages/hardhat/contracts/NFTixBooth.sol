1d4986fccb03ce35868c2fbe4008a55da170bbbf
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2022-01-30 12:28:09 +0100

    Generate json

--- packages/hardhat/contracts/NFTixBooth.sol
+++ packages/hardhat/contracts/NFTixBooth.sol
@@ -3,8 +3,14 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// https://gist.github.com/ryancharris/ed5c4f161f2ab049adf41a7f3eed2229
+import "./Base64.sol";
+
+// Final repo: https://github.com/ryancharris/nftix-demo-ui
 
 contract NFTixBooth is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -24,7 +30,44 @@ contract NFTixBooth is ERC721URIStorage {
     function mint() public {
         require(availableTickets > 0, "No more tickets available");
 
+        string[3] memory svg;
+        svg[
+            0
+        ] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y="50">';
+        svg[1] = Strings.toString(currentId.current());
+        svg[2] = "</text></svg>";
+
+        string memory image = string(abi.encodePacked(svg[0], svg[1], svg[2]));
+
+        string memory encodedImage = Base64.encode(bytes(image));
+
+        console.log("encodedImage", encodedImage);
+        // data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj48dGV4dCB5PSI1MCI+MTwvdGV4dD48L3N2Zz4=
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{ "name": "NFTix #',
+                        Strings.toString(currentId.current()),
+                        '", "description": "A NFT-powered ticketing system", ',
+                        '"traits": [{ "trait_type": "Checked In", "value": "false" }, { "trait_type": "Purchased", "value": "true" }], ',
+                        '"image": "data:image/svg+xml;base64,',
+                        encodedImage,
+                        '" }'
+                    )
+                )
+            )
+        );
+
+        string memory tokenURI = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
         _safeMint(msg.sender, currentId.current());
+        _setTokenURI(currentId.current(), tokenURI);
+        console.log("tokenURI", tokenURI);
+
         currentId.increment();
         // availableTickets = availableTickets - 1;
         availableTickets--;
