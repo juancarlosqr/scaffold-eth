c079e41c92e89a2e676a32ae38e14b3e269d0ca7
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2022-01-30 11:20:20 +0100

    Add sale active bool

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,5 +3,15 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 contract YourContract {
+    bool public saleIsActive = false;
+
     constructor() {}
+
+    function openSale() public {
+        saleIsActive = true;
+    }
+
+    function closeSale() public {
+        saleIsActive = false;
+    }
 }
