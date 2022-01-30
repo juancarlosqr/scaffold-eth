ee6e8f71aa5b52f7f534b4d0177613c991d829bd
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2022-01-30 11:32:10 +0100

    Extend contract with openzeppelin class

--- packages/hardhat/contracts/NFTixBooth.sol
+++ packages/hardhat/contracts/NFTixBooth.sol
@@ -2,10 +2,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 //SPDX-License-Identifier: MIT
 
-contract YourContract {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTixBooth is ERC721URIStorage {
     bool public saleIsActive = false;
 
-    constructor() {}
+    constructor() ERC721("NFTix", "NFTX") {}
 
     function openSale() public {
         saleIsActive = true;
